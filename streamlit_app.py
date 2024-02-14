4570fec689009620d7dc965f7a06c65178f258cb
mei7798 <160050548+mei7798@users.noreply.github.com>
2024-02-14 11:30:11 -0600

    Fix 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 """
 import pandas as pd
+import streamlit 
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
