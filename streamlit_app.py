54b6900b8e9736ff938d8201fe1eb555403c7e39
mei7798 <160050548+mei7798@users.noreply.github.com>
2024-02-14 11:16:30 -0600

    streamlit dataframe display

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit 
+"""import streamlit 
 
 streamlit.title('Hello world!')
 
@@ -6,3 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+"""
+import pandas 
+my_fruit_list = pandas.read("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
