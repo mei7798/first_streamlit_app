f9c2de54d969c5a78ec4f34ebee7c4ba5c15046f
mei7798 <160050548+mei7798@users.noreply.github.com>
2024-02-14 11:32:26 -0600

    multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-"""import streamlit 
+import pandas as pd
+import streamlit 
 
 streamlit.title('Hello world!')
 
@@ -6,9 +7,9 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-"""
-import pandas as pd
-import streamlit 
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
