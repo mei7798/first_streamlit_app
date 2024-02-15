3451825c05b9bd2f0567c4b0fc2a763ff5711599
mei7798 <160050548+mei7798@users.noreply.github.com>
2024-02-15 13:55:32 -0600

    commented insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,4 @@ if streamlit.button('Get Fruit Load List'):
 fruit_choice2 = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 streamlit.write('Thanks for adding', fruit_choice2)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
