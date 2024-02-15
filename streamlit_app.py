ab80c6ddacd3bc5d5b6c44b797d8983d678d24ce
mei7798 <160050548+mei7798@users.noreply.github.com>
2024-02-15 13:31:17 -0600

    try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+else:
+streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
@@ -34,6 +36,8 @@ fruityvice_normalized = pd.json_normalize (fruityvice_response.json())
 #output it the screen as a table 
 streamlit.dataframe(fruityvice_normalized) 
 
+except URLError as e:
+  streamlit.error()
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
