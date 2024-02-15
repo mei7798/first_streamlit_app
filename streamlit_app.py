c486b655ffdee02296148a2c8b4c077f4b830eb8
mei7798 <160050548+mei7798@users.noreply.github.com>
2024-02-15 13:33:49 -0600

    more indents

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,16 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-  streamlit.write('The user entered', fruit_choice)
-  
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  #streamlit.text(fruityvice_response.json())
-  
-  #take the json version of the response and normalize it 
-  fruityvice_normalized = pd.json_normalize (fruityvice_response.json())
-  
-  #output it the screen as a table 
-  streamlit.dataframe(fruityvice_normalized) 
+    streamlit.write('The user entered', fruit_choice)
+    
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    
+    #take the json version of the response and normalize it 
+    fruityvice_normalized = pd.json_normalize (fruityvice_response.json())
+    
+    #output it the screen as a table 
+    streamlit.dataframe(fruityvice_normalized) 
 
 except URLError as e:
   streamlit.error()
